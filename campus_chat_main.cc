c50e96dd42d15f58a35b0b96936260d46959c6fe
Sunshine <guang334419520@126.com>
2019-03-24 18:34:01 +0800

    完成基础连接

--- campus_chat_main.cc
+++ campus_chat_main.cc
@@ -1,4 +1,14 @@
 
+#include "server/server.h"
+
 int main(int argc, char* argv[]) {
+    const char host[] = "127.0.0.1";
+    std::size_t thread_count = 4;
+    auto endpoint = boost::asio::ip::tcp::endpoint(
+            boost::asio::ip::address::from_string(host), 8801);
+    cchat::Server server(thread_count, endpoint);
+    server.Start();
+    server.Run();
+
     return 0;
 }
